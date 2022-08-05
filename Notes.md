159ae8b270f2efeaf37b113e5ddeb90ec9cdf685
tihii8898 <87133096+tihii8898@users.noreply.github.com>
2022-08-06 02:07:04 +0700

    from scratch to sidebar

---
+++ Notes.md
@@ -0,0 +1,11 @@
+# **TIKTIK APP BASE LEAR FROM JAVASCRIPT MASTERY**
+
+This project is shared by Javascript Mastery. As a tiktok web clone and i just learn from it. Thank for watching <3
+## ABOUT
+
+ - Youtube Channel: https://www.youtube.com/c/JavaScriptMastery
+ - Course Video: https://www.youtube.com/watch?v=CcBHZ0t2Qwc&t=220s
+## Save point to not forget
+ - [ ] Check sanity install again because I can not login Sanity when using proxy and fake 4G :3 ( https://youtu.be/CcBHZ0t2Qwc?t=601 )
+## Lastest watch
+ - Link: https://youtu.be/CcBHZ0t2Qwc?t=3201
---
+++ components/Sidebar.tsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+import { ImCancelCircle } from "react-icons/im";
+import { AiFillHome, AiOutlineHome, AiOutlineMenu } from "react-icons/ai";
+import { TbLayoutSidebarLeftCollapse } from "react-icons/tb";
+import { CgProfile } from "react-icons/cg";
+import Link from "next/link";
+import GoogleLogin from "react-google-login";
+
+const Sidebar = () => {
+  const [showSidebar, setShowSidebar] = useState(true);
+  const userProfile = false;
+  const normalLink =
+    "flex items-center gap-3 p-3 justify-center xl:justify-start  cursor-pointer font-semibold text-[#F51997] rounded hover:bg-primary";
+  return (
+    <div>
+      <div
+        className="block m-2 ml-4 mt-3 text-xl"
+        onClick={() => setShowSidebar((prev) => !prev)}
+      >
+        {showSidebar ? <TbLayoutSidebarLeftCollapse /> : <AiOutlineMenu />}
+      </div>
+      {showSidebar && (
+        <div className="xl:w-400 w-20 flex flex-col justify-start mb-10 border-r-2 border-gray-10 xl:border-0 p-3">
+          <div className="xl:border-b-2 border-gray-100 xl:pb-4">
+            <Link href="/">
+              <div className={normalLink}>
+                <p className="text-2xl">
+                  <AiOutlineHome />
+                </p>
+                <span className="text-xl hidden text-black xl:block">Home</span>
+              </div>
+            </Link>
+          </div>
+          {!userProfile ? (
+            <div className="px-2 py-4 hidden xl:block">
+              <p className="text-gray-400">Log in to like and comment</p>
+              <div className="pr-4 py-2">
+                <GoogleLogin
+                  render={(renderProps) => (
+                    <button
+                      className="bg-white text-lg text-[#F51997] cursor-pointer border-[1px] border-[#F51997] px-4 py-2 rounded-md w-full hover:text-white text-semibold mt-3 hover:bg-[#F51997]"
+                      onClick={renderProps.onClick}
+                      disabled={renderProps.disabled}
+                    >
+                      Login by GOOGLE
+                    </button>
+                  )}
+                  clientId=""
+                  onSuccess={() => {}}
+                  onFailure={() => {}}
+                  cookiePolicy="single-host-origin"
+                />
+              </div>
+            </div>
+          ) : (
+            <div>
+              <CgProfile />
+            </div>
+          )}
+        </div>
+      )}
+      <div></div>
+    </div>
+  );
+};
+
+export default Sidebar;
